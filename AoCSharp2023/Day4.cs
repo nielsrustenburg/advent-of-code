bcde4cb3ef6065f64e4e34a7845084aea07e4e52
niels.rustenburg <niels.rustenburg@gmail.com>
2023-12-04 15:00:49 +0100

    day4

---
+++ AoCSharp2023/Day4.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCSharp2023
+{
+    internal class Day4
+    {
+
+        public Day4()
+        {
+            string[] lines = File.ReadAllLines("./inputs/4.txt");
+
+            Console.WriteLine(EvaluateAllCards(lines));
+            Console.WriteLine(EvaluateAllCards2(lines));
+        }
+
+        (HashSet<int> winning, List<int> numbersYouHave) GetScratchCard(string line)
+        {
+            var nums = line.Split(": ")[1];
+            var splitByLine = nums.Split(" | ");
+            var winning = splitByLine[0].Split(' ').Where(x => x != "").Select(x => int.Parse(x)).ToHashSet();
+            var numbersYouHave = splitByLine[1].Split(' ').Where(x => x != "").Select(x => int.Parse(x)).ToList();
+            return (winning, numbersYouHave);
+        }
+
+        int GetPoints(HashSet<int> winning, IList<int> numbersYouHave)
+        {
+            var amountOfWinners = numbersYouHave.Where(x => winning.Contains(x)).ToList();
+            if (amountOfWinners.Any())
+            {
+                return 1 << (amountOfWinners.Count - 1);
+            }
+            return 0;
+        }
+
+        int EvaluateAllCards(IList<string> cards)
+        {
+            List<int> points = new List<int>();
+            foreach (var card in cards)
+            {
+                var (win, have) = GetScratchCard(card);
+                points.Add(GetPoints(win, have));
+            }
+            return points.Sum();
+        }
+
+        int EvaluateAllCards2(IList<string> cards)
+        {
+            List<int> copies = cards.Select(_ => 1).ToList();
+            for(int i = 0; i < cards.Count; i++)
+            {
+                var card = cards[i];
+                var (win, have) = GetScratchCard(card);
+                var amountOfWinners = have.Where(x => win.Contains(x)).ToList();
+                for (int j = 0; j < amountOfWinners.Count && i+j+1 < copies.Count; j++)
+                {
+                    copies[i + j+1] += copies[i];
+                }
+            }
+            return copies.Sum();
+        }
+    }
+}
--- AoCSharp2023/Program.cs
+++ AoCSharp2023/Program.cs
@@ -3,6 +3,16 @@ using AoCSharp2023;
 
 Console.WriteLine("Hello, World!");
 
+var testLines = new List<string>()
+{
+"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+};
+
 //new Day1();
 //var d2 = new Day2();
-var d3 = new Day3();
+var d4 = new Day4();
