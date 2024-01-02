3711d232fda4675f41cc7171c4ca27d32c2ca6a1
niels.rustenburg <niels.rustenburg@gmail.com>
2024-01-02 14:44:46 +0100

    day6 + day7 p1

---
+++ AoCSharp2023/Day6.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCSharp2023
+{
+    internal class Day6
+    {
+        public Day6()
+        {
+            string[] lines = File.ReadAllLines("./inputs/6.txt");
+            Console.WriteLine(Part1(lines));
+            Console.WriteLine(Part2(lines));
+        }
+
+        int Part1(IList<string> lines)
+        {
+            var races = ParseInput(lines[0], lines[1]);
+            return races.Select(race => NumberOfRecordBreakingStrategies(race.totalTime, race.recordDistance)).Aggregate(1, (a, b) => a * b);
+        }
+
+        long Part2(IList<string> lines)
+        {
+            var race = ParseInput2(lines[0], lines[1]);
+            return NumberOfRecordBreakingStrategiesLong(race.totalTime, race.recordDistance);
+        }
+
+
+        List<(int totalTime, int recordDistance)> ParseInput(string timeLine, string distLine)
+        {
+            var times = timeLine.Split(':')[1].Split(' ').Where(x => x != string.Empty).Select(x => int.Parse(x)).ToList();
+            var distances = distLine.Split(':')[1].Split(' ').Where(x => x != string.Empty).Select(x => int.Parse(x)).ToList();
+            var result = new List<(int totalTime, int recordDistance)>();
+            for (int i = 0; i < times.Count; i++)
+            {
+                result.Add((times[i], distances[i]));
+            }
+            return result;
+        }
+
+        (long totalTime, long recordDistance) ParseInput2(string timeLine, string distLine)
+        {
+            var time = long.Parse(timeLine.Split(':')[1].Replace(" ", ""));
+            var distance = long.Parse(distLine.Split(':')[1].Replace(" ", ""));
+            return (time, distance);
+        }
+
+        long NumberOfRecordBreakingStrategiesLong(long totalTime, long distanceToBeat)
+        {
+            // Find the first winning strategy, anything between that and the inverse strategy should be winning
+            var currentBestChargeTime = totalTime / 2;
+            bool beatsRecord = (totalTime - currentBestChargeTime) * currentBestChargeTime > distanceToBeat;
+            if (!beatsRecord)
+            {
+                return 0;
+            }
+
+            var currentChargeTime = currentBestChargeTime;
+            long stepSize = currentChargeTime / 2;
+            while(stepSize > 1)
+            {
+                if (beatsRecord)
+                {
+                    currentChargeTime -= stepSize;
+                } else
+                {
+                    currentChargeTime += stepSize;
+                }
+
+                beatsRecord = (totalTime - currentChargeTime) * currentChargeTime > distanceToBeat;
+                stepSize = stepSize / 2;
+            }
+
+            // I think its likely that the above strategy can already reach the number with some proper reasoning, but dividing by 2 on integers might mess me up so to be safe, after approaching the edge going to find it iteratively
+            while (beatsRecord)
+            {
+                currentChargeTime--;
+                beatsRecord = (totalTime - currentChargeTime) * currentChargeTime > distanceToBeat;
+            }
+
+            while (!beatsRecord)
+            {
+                currentChargeTime++;
+                beatsRecord = (totalTime - currentChargeTime) * currentChargeTime > distanceToBeat;
+            }
+
+            return (totalTime - currentChargeTime) - currentChargeTime + 1; // All between currentChargeTime and inverse 
+        }
+
+        int NumberOfRecordBreakingStrategies(int totalTime, int distanceToBeat)
+        {
+            List<int> winningStrategies = new List<int>();
+            // TODO: can consider doing half work because dist is chargeTime * remaining time, so if 3 chargetime 4 remaining is winning then so is 4 charge 3 remaining
+            for (int i = 0; i < totalTime; i++)
+            {
+                var distance = DistanceTravelled(i, totalTime);
+                if (distance > distanceToBeat)
+                {
+                    winningStrategies.Add(i);
+                }
+            }
+            return winningStrategies.Count();
+        }
+
+        int DistanceTravelled(int secondsCharged, int totalTime)
+        {
+            return (totalTime - secondsCharged) * secondsCharged;
+        }
+    }
+}
---
+++ AoCSharp2023/Day7.cs
@@ -0,0 +1,137 @@
+﻿namespace AoCSharp2023
+{
+    internal class Day7
+    {
+        public Day7()
+        {
+            string[] lines = File.ReadAllLines("./inputs/7.txt");
+            Console.WriteLine(Part1(lines));
+            //Console.WriteLine(Part2(lines));
+        }
+
+        long Part1(string[] lines)
+        {
+            var handsAndBids = lines.Select(l => ParseLine(l)).ToList();
+
+            var orderedHandsAndBids = handsAndBids.OrderByDescending(l => l.Hand).ToList();
+            long total = 0;
+            for(int i = 0; i < orderedHandsAndBids.Count; i++)
+            {
+                total += orderedHandsAndBids[i].Bid * (i + 1);
+            }
+            return total;
+        }
+
+        (Hand Hand, int Bid) ParseLine(string line)
+        {
+            var split = line.Split(' ');
+            var hand = new Hand(split[0]);
+            var bid = int.Parse(split[1]);
+            return (hand, bid);
+        }
+    }
+
+    internal class Hand : IComparable<Hand>
+    {
+        public Hand(string hand)
+        {
+            if (hand.Length != 5) throw new ArgumentOutOfRangeException("Only accept hands of 5 cards");
+
+            Cards = hand.Select(c => CharToCard(c)).ToArray();
+        }
+        Card[] Cards; 
+        /// A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2
+        HandType Type
+        {
+            get
+            {
+                var buckets = Cards.GroupBy(c => c);
+                if (buckets.Count() == 1) return HandType.FiveOfAKind;
+                if (buckets.Count() == 5) return HandType.HighCard;
+                if (buckets.Count() == 4) return HandType.OnePair;
+                if (buckets.Count() == 3)
+                {
+                    return buckets.Any(b => b.Count() == 3) ? HandType.ThreeOfAKind : HandType.TwoPair;
+                }
+                if (buckets.Count() == 2)
+                {
+                    return buckets.Any(b => b.Count() == 3) ? HandType.FullHouse : HandType.FourOfAKind;
+                }
+                throw new Exception("Should not reach this for a 5-card hand unless I'm making a mistake..");
+            }
+        }
+        /// Five of a kind, where all five cards have the same label: AAAAA
+        /// Four of a kind, where four cards have the same label and one card has a different label: AA8AA
+        /// Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
+        /// Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+        /// Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+        /// One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+        /// High card, where all cards' labels are distinct: 23456
+        /// 
+
+
+        internal enum HandType
+        {
+            FiveOfAKind,
+            FourOfAKind,
+            FullHouse,
+            ThreeOfAKind,
+            TwoPair,
+            OnePair,
+            HighCard,
+        }
+
+        internal enum Card
+        {
+            Ace,
+            King,
+            Queen,
+            Jack,
+            Ten,
+            Nine,
+            Eight,
+            Seven,
+            Six,
+            Five,
+            Four,
+            Three,
+            Two,
+        }
+
+        public int CompareTo(Hand other)
+        {
+            var typeComparison = Type.CompareTo(other.Type);
+            if (typeComparison == 0)
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                    var cardComparison = Cards[i].CompareTo(other.Cards[i]);
+                    if(cardComparison != 0) return cardComparison;
+                }
+                return 0;
+            }
+            else return typeComparison;
+        }
+
+        internal Card CharToCard(char c)
+        {
+            return c switch
+            {
+                'A' => Card.Ace,
+                'K' => Card.King,
+                'Q' => Card.Queen,
+                'J' => Card.Jack,
+                'T' => Card.Ten,
+                '9' => Card.Nine,
+                '8' => Card.Eight,
+                '7' => Card.Seven,
+                '6' => Card.Six,
+                '5' => Card.Five,
+                '4' => Card.Four,
+                '3' => Card.Three,
+                '2' => Card.Two,
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
